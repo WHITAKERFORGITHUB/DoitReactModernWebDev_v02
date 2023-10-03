7d4a0ec9a67522acfa9f5abb2ff4408277f76661
WHITAKERFORGITHUB <hun1246@hanmail.net>
2023-10-03 22:14:38 +0900

    .gitignore 추가

--- ch02/ch02_1/src/index.tsx
+++ ch02/ch02_1/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const pVirtualDOM = React.createElement('p', null, 'Hello virtual Sangwoo DOM')
+const pVirtualDOM = React.createElement('p', null, 'Hello virtual Sangwoo DOM 상우야 이거 가상돔이야')
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(pVirtualDOM)
